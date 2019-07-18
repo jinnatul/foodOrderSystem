cca5dc47b582fdb63521a695d609ebe2811171c6
jinnatul <morolswediu@gmail.com>
2019-07-19 01:37:20 +0600

    add animation

--- resource/js/javascript.js
+++ resource/js/javascript.js
@@ -32,9 +32,3 @@ function scrollFunction() {
     document.getElementById("go_btn").style.display = "none";
   }
 }
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
